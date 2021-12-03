625b1d995e406ccb938082b99092ac4fde5bbd11
abrar-hossain <abrarmashuk@gmail.com>
2021-12-03 15:48:57 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Projects from './Pages/Projects/Projects';
 import ProblemSolving from './Pages/ProblemSolving/ProblemSolving';
 import Navbars from './Pages/Navbars/Navbars';
 import Blog from './Pages/Blog/Blog';
-import Details from './Pages/Details/Details';
 import About from './Pages/About/About';
 
 function App() {
@@ -24,7 +23,6 @@ function App() {
         <Route path="about/" element={<About />} />
         <Route path="contact/" element={<Contact />} />
         <Route path="projects/" element={<Projects />} />
-        <Route path="detail/" element={<Details />} />
         <Route path="problemSolving/" element={<ProblemSolving />} />
         <Route path="blog/" element={<Blog />} />
       </Routes>
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,12 +3,15 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1 className='m-5 p-5 text-center'>About Myself:</h1>
-            <h6 className='m-5 text-center'>
-                Hi All,  My name is abrar. I am from Dhaka, Bangladesh. My dream is to become a good programmer. <br />I love to code. Coding is my passion. I love to solve programming problems.<br />
-                Besides, coding I love to watch movies, play football, cricket. I love to read books.Travelling is my most favorite hobby.<br />
+            <h1 className='m-5 text-center'>About Myself:</h1>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <h6 className="about text-justify">
+                    Hi All,  My name is abrar. I am from Dhaka, Bangladesh. My dream is to become a good programmer. <br />I love to code. Coding is my passion. I always try to solve programming problems.<br />
+                    Besides, coding watch movies, playing football, cricket. In my spare time, I try to read books. Travelling is my most favorite hobby.<br />
                 Sometimes, I try to solve some math-related problems. Solving the riddle boosts my energy.
             </h6>
+            </div>
+
         </div>
     );
 };
